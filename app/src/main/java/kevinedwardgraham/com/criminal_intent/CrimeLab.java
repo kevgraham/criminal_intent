1643d8df77798a1d3ebd7117a4c54b3af2700e91
Kevin Graham <kevin.edward.graham@gmail.com>
2018-08-15 07:52:11 -0400

    singleton data source

---
+++ app/src/main/java/kevinedwardgraham/com/criminal_intent/CrimeLab.java
@@ -0,0 +1,63 @@
+package kevinedwardgraham.com.criminal_intent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Singleton Crime Datastore
+ */
+public class CrimeLab {
+
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    private CrimeLab(Context content) {
+        // temp data
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0);
+            mCrimes.add(crime);
+        }
+    }
+
+    /**
+     * Crimelab Singleton Factory
+     * @param context
+     * @return
+     */
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    /**
+     * Get all crimes
+     * @return
+     */
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    /**
+     * Get the crime with a specific UUID
+     * @param id
+     * @return
+     */
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+
+}
