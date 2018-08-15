4cf5adbbe67f0cc759c2ece4be6a4b3d1d0cfa2c
Kevin Graham <kevin.edward.graham@gmail.com>
2018-08-15 08:51:20 -0400

    implement CrimeListActivity with RecyclerView

---
+++ app/src/main/java/kevinedwardgraham/com/criminal_intent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package kevinedwardgraham.com.criminal_intent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
